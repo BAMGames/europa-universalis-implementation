32ffbd32b227773b3c8ef7f5c5cc5bf5bcd70882
FogiaFr <fogia.fr@gmail.com>
2015-04-29 10:06:07 +0200

    Addition of the ROTW province

---
+++ client/eu-client-service/src/main/java/com/mkl/eu/client/service/vo/ref/RotwProvince.java
@@ -0,0 +1,10 @@
+package com.mkl.eu.client.service.vo.ref;
+
+/**
+ * Rotw province. A tile on the rotw board that can contains counters.
+ * TODO check if still used later.
+ *
+ * @author MKL.
+ */
+public class RotwProvince extends AbstractProvince {
+}
---
+++ service/eu-service-service/src/main/java/com/mkl/eu/service/service/persistence/oe/ref/RotwProvinceEntity.java
@@ -0,0 +1,16 @@
+package com.mkl.eu.service.service.persistence.oe.ref;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+/**
+ * European province. A tile on the european board that can contains counters.
+ *
+ * @author MKL.
+ */
+@Entity
+@Table(name = "R_PROVINCE_ROTW")
+@PrimaryKeyJoinColumn(name = "ID")
+public class RotwProvinceEntity extends AbstractProvinceEntity {
+}
